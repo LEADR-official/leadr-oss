5d553d33b293b7a9c06589b45fc7bcbba19898c4
Barney Jackson <barney@leadr.gg>
2026-01-10 22:18:15 +0100

    install xdist and pytest-cov for parallelised testing, and update test.sh & ci

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-PYTHONDONTWRITEBYTECODE=1 ENV=TEST uv run coverage run -m pytest -p no:pastebin -p no:nose -p no:doctest $@
+PYTHONDONTWRITEBYTECODE=1 ENV=TEST uv run pytest \
+	--cov=src \
+	--cov-report html \
+	tests \
+	-n auto -p no:pastebin -p no:nose -p no:doctest \
+	$@
 
-uv run coverage html
-uv run coverage report --skip-covered --sort=Cover
+uv run coverage report --sort=Cover --skip-covered
